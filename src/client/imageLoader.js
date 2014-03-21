c0d8de4814bd2018772173e00320bcfc0d3908f7
Chris Hafey <chafey@gmail.com>
2014-03-20 19:13:15 -0500

    extracted the embedded example image code out of the core library and updated all examples to include it

--- src/client/imageLoader.js
+++ src/client/imageLoader.js
@@ -8,12 +8,11 @@ var cornerstone = (function (cornerstone, csc) {
 
     function loadImage(imageId) {
         if(imageCache[imageId] === undefined) {
-            // currently hardcoded to use example images only
-            if(imageId.substring(0, 10) === 'example://')
-            {
-                var image = cornerstone.getExampleImage(imageId);
-                return image;
-            }
+            var colonIndex = imageId.indexOf(":");
+            var scheme = imageId.substring(0, colonIndex);
+            var loader = cornerstone.imageIdSchemes[scheme];
+            var image = loader(imageId);
+            return image;
         }
         else {
             return imageCache[imageId];
