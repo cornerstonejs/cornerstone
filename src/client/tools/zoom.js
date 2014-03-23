93f7f7cbf9c86d7f3c4342d86e74f4dee9058df6
Chris Hafey <chafey@gmail.com>
2014-03-23 15:57:30 -0500

    zoom tool now keeps the image centered on the where the cursor is at when the zoom begins

--- src/client/tools/zoom.js
+++ src/client/tools/zoom.js
@@ -5,8 +5,14 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
     }
 
     function zoom(element, whichMouseButton){
+
         if(whichMouseButton == 0) {
+
+
             $(element).on('mousewheel DOMMouseScroll', function(e) {
+
+                var startingCoords = cornerstone.pageToImage(element, e.pageX, e.pageY);
+
                 // Firefox e.originalEvent.detail > 0 scroll back, < 0 scroll forward
                 // chrome/safari e.originalEvent.wheelDelta < 0 scroll back, > 0 scroll forward
                 var ticks = 0;
@@ -24,8 +30,15 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
                 var factor = oldFactor + ticks;
                 var scale = Math.pow(power, factor);
                 viewport.scale = scale;
-                cornerstone.setViewport(element, viewport);
 
+                var ee = cornerstone.getEnabledElement(element);
+                ee.viewport.scale = scale;
+
+                // now adjust the centerX and Y
+                var newCoords = cornerstone.pageToImage(element, e.pageX, e.pageY);
+                viewport.centerX -= startingCoords.x - newCoords.x;
+                viewport.centerY -= startingCoords.y - newCoords.y;
+                cornerstone.setViewport(element, viewport);
 
                 //prevent page fom scrolling
                 return false;
@@ -39,13 +52,18 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
                 var mouseButton = e.which;
 
                 if(mouseButton == whichMouseButton) {
+
+                    var startingCoords = cornerstone.pageToImage(element, e.pageX, e.pageY);
+                    var startPageX = e.pageX;
+                    var startPageY = e.pageY;
                     $(document).mousemove(function(e) {
                         var deltaX = e.pageX - lastX,
                             deltaY = e.pageY - lastY ;
+
                         lastX = e.pageX;
                         lastY = e.pageY;
 
-                        var pow = 1.3;
+                        var pow = 1.7;
 
                         var viewport = cornerstone.getViewport(element);
                         var ticks = deltaY/100;
@@ -53,7 +71,16 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
                         var factor = oldFactor + ticks;
                         var scale = Math.pow(pow, factor);
                         viewport.scale = scale;
+
+                        var ee = cornerstone.getEnabledElement(element);
+                        ee.viewport.scale = scale;
+
+                        // now adjust the centerX and Y
+                        var newCoords = cornerstone.pageToImage(element, startPageX, startPageY);
+                        viewport.centerX -= startingCoords.x - newCoords.x;
+                        viewport.centerY -= startingCoords.y - newCoords.y;
                         cornerstone.setViewport(element, viewport);
+
                     });
 
                     $(document).mouseup(function(e) {
