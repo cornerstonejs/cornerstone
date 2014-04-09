4b4ded85bfe5e4df6deb4a1878a0709ce05aa919
Chris Hafey <chafey@gmail.com>
2014-04-09 12:45:46 -0500

    added CustomEvent polyfill for IE9-11

--- dist/cornerstone.js
+++ dist/cornerstone.js
@@ -4,6 +4,46 @@ var cornerstoneCore = (function (cornerstoneCore) {
         cornerstoneCore = {};
     }
 
+
+    function ie_ver(){
+        var iev=0;
+        var ieold = (/MSIE (\d+\.\d+);/.test(navigator.userAgent));
+        var trident = !!navigator.userAgent.match(/Trident\/7.0/);
+        var rv=navigator.userAgent.indexOf("rv:11.0");
+
+        if (ieold) iev=new Number(RegExp.$1);
+        if (navigator.appVersion.indexOf("MSIE 10") != -1) iev=10;
+        if (trident&&rv!=-1) iev=11;
+
+        return iev;
+    }
+
+    var ieVer = ie_ver();
+    if(ieVer <= 11) {
+        (function () {
+            function CustomEvent ( event, params ) {
+                params = params || { bubbles: false, cancelable: false, detail: undefined };
+                var evt = document.createEvent( 'CustomEvent' );
+                evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+                return evt;
+            };
+
+            CustomEvent.prototype = window.Event.prototype;
+
+            window.CustomEvent = CustomEvent;
+        })();
+    }
+
+
+    return cornerstoneCore;
+}(cornerstoneCore));
+
+
+var cornerstoneCore = (function (cornerstoneCore) {
+    if(cornerstoneCore === undefined) {
+        cornerstoneCore = {};
+    }
+
     function distanceSquared(pt1, pt2)
     {
         var dx = pt1.x - pt2.x;
