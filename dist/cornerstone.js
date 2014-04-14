0dcb2c03901075a275339d33988a388f9bd3afb2
Chris Hafey <chafey@gmail.com>
2014-04-13 19:44:01 -0500

    removing example directory from bower package

--- dist/cornerstone.js
+++ dist/cornerstone.js
@@ -1,4 +1,4 @@
-/*! cornerstone - v0.1.0 - 2014-04-13 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstone */
+/*! cornerstone - v0.1.2 - 2014-04-13 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstone */
 var cornerstone = (function (cornerstone) {
 
     "use strict";
