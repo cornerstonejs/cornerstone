53f25a9888bbcf8bffc06746a41b42a64691bd5c
chafey <chafey@lury.net>
2014-05-07 20:48:53 -0500

    discovered a bug where pageToPixel did not apply the DPI properly.  The fix for this is to divide the scale by the DPI in pageToPixel - but I realized that this causes incorrect results on systems with mixed dpi displays.  This functionality has therefore been disabled until a safe and reliable solution can be found

--- src/resize.js
+++ src/resize.js
@@ -13,13 +13,27 @@ var cornerstone = (function (cornerstone) {
     {
         // the device pixel ratio is 1.0 for normal displays and > 1.0
         // for high DPI displays like Retina
+        /*
+
+        This functionality is disabled due to buggy behavior on systems with mixed DPI's.  If the canvas
+        is created on a display with high DPI (e.g. 2.0) and then the browser window is dragged to
+        a different display with a different DPI (e.g. 1.0), the canvas is not recreated so the pageToPixel
+        produces incorrect results.  I couldn't find any way to determine when the DPI changed other than
+        by polling which is not very clean.  If anyone has any ideas here, please let me know, but for now
+        we will disable this functionality.  We may want
+        to add a mechanism to optionally enable this functionality if we can determine it is safe to do
+        so (e.g. iPad or iPhone or perhaps enumerate the displays on the system.  I am choosing
+        to be cautious here since I would rather not have bug reports or safety issues related to this
+        scenario.
+
         var devicePixelRatio = window.devicePixelRatio;
         if(devicePixelRatio === undefined) {
             devicePixelRatio = 1.0;
         }
+        */
 
-        canvas.width = element.clientWidth * devicePixelRatio;
-        canvas.height = element.clientHeight * devicePixelRatio;
+        canvas.width = element.clientWidth;
+        canvas.height = element.clientHeight;
         canvas.style.width = element.clientWidth + "px";
         canvas.style.height = element.clientHeight + "px";
     }
