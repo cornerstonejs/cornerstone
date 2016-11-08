cf45c1a89dcdcb5914ef159552a4f74ab474592f
Evren Ozkan <evren217@gmail.com>
2016-11-08 14:49:21 -0500

    Display the images that have only VOI LUT Sequence - Shift the stored VOI LUT values by "numBitsPerEntryInVOILutData - bitsStored" instead of "numBitsPerEntryInVOILutData - 8" - Implement the default linear function for the images that have only VOI LUT Sequence to apply Window Width and Window Center changed by user

--- src/internal/voiLUT.js
+++ src/internal/voiLUT.js
@@ -12,29 +12,42 @@
     }
   }
 
-  function generateNonLinearVOILUT(voiLUT) {
-    var shift = voiLUT.numBitsPerEntry - 8;
+  function generateNonLinearVOILUT(windowWidth, windowCenter, bitsStored, minPixelValue, maxPixelValue, voiLUT) {
+    // Shift the stored value based on bitsStored, not always 8 bits
+    var shift = voiLUT.numBitsPerEntry - (bitsStored || 8);
     var minValue = voiLUT.lut[0] >> shift;
     var maxValue = voiLUT.lut[voiLUT.lut.length -1] >> shift;
     var maxValueMapped = voiLUT.firstValueMapped + voiLUT.lut.length - 1;
     return function(modalityLutValue) {
+      var voiLutValue;
       if(modalityLutValue < voiLUT.firstValueMapped) {
-        return minValue;
+        voiLutValue = minValue;
       }
-      else if(modalityLutValue >= maxValueMapped)
-      {
-        return maxValue;
+      else if(modalityLutValue >= maxValueMapped) {
+        voiLutValue = maxValue;
       }
-      else
-      {
-        return voiLUT.lut[modalityLutValue - voiLUT.firstValueMapped] >> shift;
+      else {
+        voiLutValue = voiLUT.lut[modalityLutValue - voiLUT.firstValueMapped] >> shift;
+      }
+
+      // Apply the linear conversion from stored pixel values (after any Modality LUT or Rescale Slope and
+      // Intercept specified in the IOD have been applied) to the values to be displayed based on Window Width
+      // and Window Center
+      if (voiLutValue <= windowCenter - 0.5 - (windowWidth-1) / 2) {
+        return 0;
+      }
+      else if (voiLutValue > windowCenter - 0.5 + (windowWidth-1) / 2) {
+        return 255;
+      }
+      else {
+        return ((voiLutValue - (windowCenter-0.5)) / (windowWidth-1) + 0.5) * (maxPixelValue-minPixelValue) + minPixelValue;
       }
     }
   }
 
-  function getVOILUT(windowWidth, windowCenter, voiLUT) {
+  function getVOILUT(windowWidth, windowCenter, bitsStored, minPixelValue, maxPixelValue, voiLUT) {
     if(voiLUT) {
-      return generateNonLinearVOILUT(voiLUT);
+      return generateNonLinearVOILUT(windowWidth, windowCenter, bitsStored, minPixelValue, maxPixelValue, voiLUT);
     } else {
       return generateLinearVOILUT(windowWidth, windowCenter);
     }
