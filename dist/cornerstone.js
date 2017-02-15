84b1e1545cdc3cd847b97724b581107bdb19174f
Hugo Roenick <hroenick@alis-sol.puc-rio.br>
2017-02-15 08:53:17 -0200

    Updating dist files after merging with upstream/master

--- dist/cornerstone.js
+++ dist/cornerstone.js
@@ -1,4 +1,4 @@
-/*! cornerstone - v0.10.1 - 2017-02-11 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstone */
+/*! cornerstone - v0.10.1 - 2017-02-15 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstone */
 if(typeof cornerstone === 'undefined'){
     cornerstone = {
         internal : {},
