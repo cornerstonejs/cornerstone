5163f1e092133a0301d59203332837c1cee1c7be
Erik Ziegler <erik.sweed@gmail.com>
2017-03-25 09:01:22 +0100

    Wrap intensive loops in IIFE to prevent de-optimization (Fix #101)

--- src/internal/storedPixelDataToCanvasImageData.js
+++ src/internal/storedPixelDataToCanvasImageData.js
@@ -29,19 +29,22 @@
         var localPixelData = pixelData;
         var localLut = lut;
         var localCanvasImageDataData = canvasImageDataData;
-        // NOTE: As of Nov 2014, most javascript engines have lower performance when indexing negative indexes.
-        // We have a special code path for this case that improves performance.  Thanks to @jpambrun for this enhancement
-        if(minPixelValue < 0){
-            while(storedPixelDataIndex < localNumPixels) {
-                localCanvasImageDataData[canvasImageDataIndex] = localLut[localPixelData[storedPixelDataIndex++] + (-minPixelValue)]; // alpha
-                canvasImageDataIndex += 4;
+
+        (function () {
+            // NOTE: As of Nov 2014, most javascript engines have lower performance when indexing negative indexes.
+            // We have a special code path for this case that improves performance.  Thanks to @jpambrun for this enhancement
+            if(minPixelValue < 0){
+                while(storedPixelDataIndex < localNumPixels) {
+                    localCanvasImageDataData[canvasImageDataIndex] = localLut[localPixelData[storedPixelDataIndex++] + (-minPixelValue)]; // alpha
+                    canvasImageDataIndex += 4;
+                }
+            }else{
+                while(storedPixelDataIndex < localNumPixels) {
+                    localCanvasImageDataData[canvasImageDataIndex] = localLut[localPixelData[storedPixelDataIndex++]]; // alpha
+                    canvasImageDataIndex += 4;
+                }
             }
-        }else{
-            while(storedPixelDataIndex < localNumPixels) {
-                localCanvasImageDataData[canvasImageDataIndex] = localLut[localPixelData[storedPixelDataIndex++]]; // alpha
-                canvasImageDataIndex += 4;
-            }
-        }
+        })();
     }
 
     // Module exports
