dc0578a81d638d46a46f5c268cffa9ad5227d3e8
Erik Ziegler <erik.sweed@gmail.com>
2017-04-06 11:03:23 +0200

    Bump to version 0.10.5

--- dist/cornerstone.js
+++ dist/cornerstone.js
@@ -1,4 +1,4 @@
-/*! cornerstone - v0.10.4 - 2017-04-06 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstone */
+/*! cornerstone - v0.10.5 - 2017-04-06 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstone */
 if(typeof cornerstone === 'undefined'){
     cornerstone = {
         internal : {},
