cf7ebe8dc45f09cfab10df68d97e072c79b009d5
Erik Ziegler <erik.sweed@gmail.com>
2017-05-26 10:25:49 +0200

    Add Istanbul to package.json because it seems to be missing. Add test to pull in all modules so coverage HTML report is accurate

---
+++ test/coverage_test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+import * as cornerstone from '../src/index';
+
+describe('A test that pulls in all modules', function() {
+  it('pulls in all modules', function() {
+    expect(cornerstone).to.exist;
+  });
+});
