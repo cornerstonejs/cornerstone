80ab9541347cdb65fbe130f6d0fe968dfa74997e
Kai Schlamp <kai.schlamp@gmail.com>
2017-08-20 16:30:35 +0200

    Added documentation to pubSub module

--- dist/cornerstone.js
+++ dist/cornerstone.js
@@ -304,7 +304,17 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  * Original code by https://gist.github.com/fatihacet/1290216
  */
 
+/**
+ * The PubSub class that handles the publication and subscriptions of one
+ * element.
+ */
 var PubSub = function () {
+
+  /**
+   * Create a PubSub object to publish and subscribe to events.
+   * @param {*} element The element this PubSub instance is associated with
+   *                    (may be undefined if this is the global PubSub instance).
+   */
   function PubSub(element) {
     _classCallCheck(this, PubSub);
 
@@ -312,6 +322,16 @@ var PubSub = function () {
     this.subUid = -1;
     this.topics = {};
   }
+
+  /**
+   * Subscribe to events.
+   * @param {string} topic The event topic to subscribe to.
+   * @param {function} func The callback that is called when this event happens.
+   * @param {*} extras An optional argument that is passed to the func callback.
+   *
+   * @return {string} A unique token that can be used to unsubsribe from this subscription.
+   */
+
 
   _createClass(PubSub, [{
     key: "subscribe",
@@ -330,23 +350,44 @@ var PubSub = function () {
 
       return token;
     }
+
+    /**
+     * Unsubscribe from events.
+     * @param {string} token The token got during subscription.
+     *
+     * @return {bool} True if unsubscribed successfully, false if never subscribed.
+     */
+
   }, {
     key: "unsubscribe",
     value: function unsubscribe(token) {
-      for (var m in this.topics) {
-        if (this.topics[m]) {
-          for (var i = 0, j = this.topics[m].length; i < j; i++) {
-            if (this.topics[m][i].token === token) {
-              this.topics[m].splice(i, 1);
+      for (var key in this.topics) {
+        if (!this.topics[key]) {
+          continue;
+        }
 
-              return token;
+        for (var i = 0, j = this.topics[key].length; i < j; i++) {
+          if (this.topics[key][i].token === token) {
+            this.topics[key].splice(i, 1);
+
+            if (this.topics[key].length === 0) {
+              delete this.topics[key];
             }
+
+            return true;
           }
         }
       }
 
       return false;
     }
+
+    /**
+     * Unsubscribe from all event topics.
+     *
+     * @return {void}
+     */
+
   }, {
     key: "unsubscribeAll",
     value: function unsubscribeAll() {
@@ -360,6 +401,16 @@ var PubSub = function () {
         delete _this.topics[key];
       });
     }
+
+    /**
+     * Publish a new event to all subscribed listeners.
+     * @param {string} topic The event topic publish.
+     * @param {*} args Arguments that are passed to subscribed listeners.
+     *
+     * @return {bool} True if the event was successfully published to listeners,
+     *                false if no one is listening to this event topic.
+     */
+
   }, {
     key: "publish",
     value: function publish(topic, args) {
@@ -385,6 +436,14 @@ var PubSub = function () {
 
 var globalInstance = new PubSub();
 var instances = new Map();
+
+/**
+ * Fetch a pubSub instance.
+ * @param {*} element The element for which to fetch the pubSub instance.
+ *                    If not provided (or undefined) a global instance is returned.
+ *
+ * @return {PubSub} A PubSub instance.
+ */
 
 /***/ }),
 /* 2 */
--- src/pubSub.js
+++ src/pubSub.js
@@ -3,13 +3,31 @@
  * Original code by https://gist.github.com/fatihacet/1290216
  */
 
+/**
+ * The PubSub class that handles the publication and subscriptions of one
+ * element.
+ */
 class PubSub {
+
+  /**
+   * Create a PubSub object to publish and subscribe to events.
+   * @param {*} element The element this PubSub instance is associated with
+   *                    (may be undefined if this is the global PubSub instance).
+   */
   constructor (element) {
     this.element = element;
     this.subUid = -1;
     this.topics = {};
   }
 
+  /**
+   * Subscribe to events.
+   * @param {string} topic The event topic to subscribe to.
+   * @param {function} func The callback that is called when this event happens.
+   * @param {*} extras An optional argument that is passed to the func callback.
+   *
+   * @return {string} A unique token that can be used to unsubsribe from this subscription.
+   */
   subscribe (topic, func, extras) {
     if (!this.topics[topic]) {
       this.topics[topic] = [];
@@ -26,15 +44,27 @@ class PubSub {
     return token;
   }
 
+  /**
+   * Unsubscribe from events.
+   * @param {string} token The token got during subscription.
+   *
+   * @return {bool} True if unsubscribed successfully, false if never subscribed.
+   */
   unsubscribe (token) {
-    for (const m in this.topics) {
-      if (this.topics[m]) {
-        for (let i = 0, j = this.topics[m].length; i < j; i++) {
-          if (this.topics[m][i].token === token) {
-            this.topics[m].splice(i, 1);
+    for (const key in this.topics) {
+      if (!this.topics[key]) {
+        continue;
+      }
 
-            return token;
+      for (let i = 0, j = this.topics[key].length; i < j; i++) {
+        if (this.topics[key][i].token === token) {
+          this.topics[key].splice(i, 1);
+
+          if (this.topics[key].length === 0) {
+            delete this.topics[key];
           }
+
+          return true;
         }
       }
     }
@@ -42,6 +72,11 @@ class PubSub {
     return false;
   }
 
+  /**
+   * Unsubscribe from all event topics.
+   *
+   * @return {void}
+   */
   unsubscribeAll () {
     for (const i in this.topics) {
       this.topics[i].length = 0;
@@ -52,6 +87,14 @@ class PubSub {
     });
   }
 
+  /**
+   * Publish a new event to all subscribed listeners.
+   * @param {string} topic The event topic publish.
+   * @param {*} args Arguments that are passed to subscribed listeners.
+   *
+   * @return {bool} True if the event was successfully published to listeners,
+   *                false if no one is listening to this event topic.
+   */
   publish (topic, args) {
     if (!this.topics[topic]) {
       return false;
@@ -73,6 +116,13 @@ class PubSub {
 const globalInstance = new PubSub();
 const instances = new Map();
 
+/**
+ * Fetch a pubSub instance.
+ * @param {*} element The element for which to fetch the pubSub instance.
+ *                    If not provided (or undefined) a global instance is returned.
+ *
+ * @return {PubSub} A PubSub instance.
+ */
 export default function (element) {
   if (!element) {
     return globalInstance;
