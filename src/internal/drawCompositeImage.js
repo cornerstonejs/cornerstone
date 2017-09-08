b590118cb527a34729eb859d921a56a764574c3d
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-09-08 11:32:40 +0200

    previous white spaces

--- src/internal/drawCompositeImage.js
+++ src/internal/drawCompositeImage.js
@@ -75,12 +75,11 @@ function renderLayers (context, activeLayer, layers, invalidated) {
       pixelDataUpdated = restoreImage(layer.image);
     }
 
-    // Use the windowing of the updated image
-    if (pixelDataUpdated === true) {
+	if (pixelDataUpdated === true) {
       layer.viewport.voi.windowWidth = layer.image.windowWidth;
       layer.viewport.voi.windowCenter = layer.image.windowCenter;
     }
-
+	
     // If the image got updated it needs to be re-rendered
     invalidated = invalidated || pixelDataUpdated;
 
