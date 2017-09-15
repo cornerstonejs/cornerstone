4bfc60afd092fbc80afb4a840600e9b894f74abe
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-09-15 09:50:21 +0200

    trying to resolve the error "Error during loading "karma-phantomjs-launcher" plugin: Path must be a string. Received null", throwed when submiting the PR

--- src/layers.js
+++ src/layers.js
@@ -1,6 +1,3 @@
-require('phantomjs-polyfill-find');
-require('phantomjs-polyfill-find-index');
-
 import $ from './jquery.js';
 import guid from './internal/guid.js';
 import { getEnabledElement } from './enabledElements.js';
