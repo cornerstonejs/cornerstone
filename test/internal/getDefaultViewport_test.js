e380f7822f82ab319bd185867b50ca835da15ff9
André Botelho Almeida <andrebot_almeida@hotmail.com>
2017-12-08 12:41:28 -0200

    Add some more tests for internal functions (#198)
    * Adding getDefaultViewport tests for throwing error when canvas is empty and for when image is empty
    
    * Adding storedColorPixelDataToCanvasImageData tests. Bring it up to 100%
    
    * Refactored storedPixelDataToCanvasImageData_test. Completed 100% on storedPixelDataToCanvasImageData
    
    * Added storedPixelDataToCanvasImageDataColorLUT tests. Bring it up to 96%
    
    * Refactored some tests files to use should pattern. Added storedPixelDataToCanvasImageDataPseudocolorLUT tests, bring it up to 100%

---
+++ test/internal/getDefaultViewport_test.js
@@ -0,0 +1,16 @@
+import { should } from 'chai';
+import getDefaultViewport from '../../src/internal/getDefaultViewport';
+
+should();
+
+describe('getDefaultViewport', function () {
+  it('should throw an error if we don\'t have a canvas', function () {
+    getDefaultViewport.should.throw('getDefaultViewport: parameter canvas must not be undefined');
+  });
+
+  it('should return an empty viewport if no image is provided', function () {
+    const emptyViewport = getDefaultViewport({});
+
+    emptyViewport.should.have.all.keys(['scale', 'translation', 'voi', 'invert', 'pixelReplication', 'rotation', 'hflip', 'vflip', 'modalityLUT', 'voiLUT', 'colormap', 'labelmap']);
+  });
+});
---
+++ test/internal/storedColorPixelDataToCanvasImageData_test.js
@@ -0,0 +1,44 @@
+import { should } from 'chai';
+import storedColorPixelDataToCanvasImageData from '../../src/internal/storedColorPixelDataToCanvasImageData';
+
+should();
+
+describe('storedColorPixelDataToCanvasImageData', function () {
+  before(function () {
+    this.image = {
+      getPixelData() {
+        return [1, 2, 3, 4, 2, 0, 1, 0]
+      },
+      stats: {}
+    };
+    this.lut = [1, 2, 3, 4, 5, 6, 7, 8];
+  });
+
+  describe('pixel value less than 0', function () {
+    before(function () {
+      this.image.minPixelValue = -3;
+    });
+
+    it('should generate the image subtracting the minimum pixel value', function () {
+      let canvasImageDataData = [0, 0, 0, 0, 0, 0, 0, 0];
+
+      storedColorPixelDataToCanvasImageData(this.image, this.lut, canvasImageDataData);
+
+      canvasImageDataData.should.be.deep.equal([5, 6, 7, 0, 6, 4, 5, 0]);
+    });
+  });
+
+  describe('pixel value is equal to or greater than 0', function () {
+    before(function () {
+      this.image.minPixelValue = 0;
+    });
+
+    it('should generate the image just using the lookup table', function () {
+      let canvasImageDataData = [0, 0, 0, 0, 0, 0, 0, 0];
+
+      storedColorPixelDataToCanvasImageData(this.image, this.lut, canvasImageDataData);
+
+      canvasImageDataData.should.be.deep.equal([2, 3, 4, 0, 3, 1, 2, 0]);
+    });
+  });
+});
