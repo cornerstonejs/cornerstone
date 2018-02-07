a44ccffb979f81a01ad22d898a8253c5950784cd
Erik Ziegler <erik.sweed@gmail.com>
2018-02-07 10:35:21 +0100

    Update the API documentation for enable() to include the fix for #200

--- src/enable.js
+++ src/enable.js
@@ -26,6 +26,11 @@ function hasImageOrLayers (enabledElement) {
 /**
  * Enable an HTML Element for use in Cornerstone
  *
+ * - If there is a Canvas already present within the HTMLElement, and it has the class
+ * 'cornerstone-canvas', this function will use this existing Canvas instead of creating
+ * a new one. This may be helpful when using libraries (e.g. React, Vue) which don't
+ * want third parties to change the DOM.
+ *
  * @param {HTMLElement} element An HTML Element enabled for Cornerstone
  * @param {Object} options Options for the enabledElement
  *
