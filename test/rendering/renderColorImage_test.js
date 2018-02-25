0e5248c36e18ae6a38bdea56c3e958a9c8414c08
posegae <posegae@carleton.edu>
2018-02-24 19:43:43 -0600

    Update renderColorImage_test.js
    added clarifying comments, extended tests

--- test/rendering/renderColorImage_test.js
+++ test/rendering/renderColorImage_test.js
@@ -6,7 +6,10 @@ import disable from '../../src/disable.js';
 import { renderColorImage } from '../../src/rendering/renderColorImage.js';
 import { getEnabledElement } from '../../src/enabledElements.js';
 import setViewport from '../../src/setViewport.js';
-// import getCanvas from '../../src/internal/getCanvas.js';
+import draw from '../../src/draw.js';
+import {
+  addLayer
+} from '../../src/layers.js';
 
 describe('renderColorImage', function () {
   beforeEach(function () {
@@ -17,13 +20,11 @@ describe('renderColorImage', function () {
 
     enable(this.element);
 
-    const getPixelData = () => new Uint8Array([0, 255, 255, 0]);
     const enabledElement = getEnabledElement(this.element);
     const canvasContext = enabledElement.canvas.getContext('2d');
     const imageData = canvasContext.createImageData(width, height);
     const pixelData = imageData.data;
     const rnd = Math.round(Math.random() * 255);
-
     let index = 0;
 
     for (let y = 0; y < height; y++) {
@@ -33,6 +34,11 @@ describe('renderColorImage', function () {
         pixelData[index++] = 0; // BLUE
         pixelData[index++] = 255; // ALPHA
       }
+    }
+    canvasContext.putImageData(imageData, 0, 0);
+
+    function getPixelData () {
+      return pixelData;
     }
 
     this.image = {
@@ -79,21 +85,18 @@ describe('renderColorImage', function () {
     const image = this.image;
     const enabledElement = getEnabledElement(element);
 
-    // TODO: check that renderCanvas doesn't exist
-
+    // Act
     renderColorImage(enabledElement, false);
-
     const renderCanvas = enabledElement.renderingTools.renderCanvas;
 
-    // Act
-    // TODO: add additional checks?
-    // Checking that render canvas exists and that its dimensions match the image dimensions
+    // Assert
+    // Check that render canvas exists now and that its dimensions match the image dimensions
     assert.exists(renderCanvas);
     assert.equal(renderCanvas.height, image.height);
     assert.equal(renderCanvas.width, image.width);
   });
 
-  it('should create a render canvas, then recycle the existing render canvas when applied again', function () {
+  it('should reuse old render canvas when invalidated !== true', function () {
     // Arrange
     const element = this.element;
     const image = this.image;
@@ -102,15 +105,37 @@ describe('renderColorImage', function () {
     // Act
     renderColorImage(enabledElement, true);
     const renderCanvas1 = Object.assign({}, enabledElement.renderingTools.renderCanvas);
+
+    // Changing image measurements shouldn't affect renderCanvas when invalidated !== true
+    image.height = 500;
+    this.viewport.height = 500;
+
     renderColorImage(enabledElement, false);
     const renderCanvas2 = Object.assign({}, enabledElement.renderingTools.renderCanvas);
-    assert.deepEqual(renderCanvas1, renderCanvas2);
-    renderColorImage(enabledElement, true);
-    const renderCanvas3 = Object.assign({}, enabledElement.renderingTools.renderCanvas);
-    assert.deepEqual(renderCanvas1, renderCanvas3);
 
+    // Assert
+    assert.deepEqual(renderCanvas1, renderCanvas2);
   });
 
+  it('should create a new render canvas with updated measurements when invalidated === true', function () {
+    // Arrange
+    const element = this.element;
+    const image = this.image;
+    const enabledElement = getEnabledElement(element);
+
+    // Act
+    renderColorImage(enabledElement, true);
+    const renderCanvas1 = Object.assign({}, enabledElement.renderingTools.renderCanvas);
+
+    image.height = 500;
+    this.viewport.height = 500;
+
+    renderColorImage(enabledElement, true);
+    const renderCanvas2 = Object.assign({}, enabledElement.renderingTools.renderCanvas);
+
+    // Assert
+    assert.notDeepEqual(renderCanvas1, renderCanvas2);
+  });
 
   it('should fail to render color image without image or enabled element', function () {
     // Arrange
@@ -119,9 +144,66 @@ describe('renderColorImage', function () {
 
     enabledElement.image = undefined;
 
-    // Act
+    // Assert
     assert.throws(() => renderColorImage(enabledElement, false));
     assert.throws(() => renderColorImage(undefined, false), Error, 'renderColorImage: enabledElement parameter must not be undefined');
+  });
+
+  it('should successfully add color layer', function (done) {
+    // Arrange
+    // Construct an additional color image to layer
+    const width = 256;
+    const height = 256;
+    const enabledElement = getEnabledElement(this.element);
+    const canvasContext = enabledElement.canvas.getContext('2d');
+    const imageData2 = canvasContext.createImageData(width, height);
+    const pixelData2 = imageData2.data;
+    const rnd = Math.round(Math.random() * 255);
+    let index = 0;
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        pixelData2[index++] = 0; // RED
+        pixelData2[index++] = (x + rnd) % 256; // GREEN
+        pixelData2[index++] = 0; // BLUE
+        pixelData2[index++] = 255; // ALPHA
+      }
+    }
+    canvasContext.putImageData(imageData2, 0, 0);
+
+    function getPixelData2 () {
+      return pixelData2;
+    }
+
+    this.image2 = {
+      imageId: 'exampleImageId2',
+      minPixelValue: 0,
+      maxPixelValue: 255,
+      slope: 1.0,
+      intercept: 0,
+      windowCenter: 127,
+      windowWidth: 256,
+      getPixelData: getPixelData2,
+      rows: height,
+      columns: width,
+      height,
+      width,
+      color: true,
+      sizeInBytes: width * height * 2
+    };
+
+    addLayer(this.element, this.image2, {
+      opacity: 0.7
+    });
+
+    this.element.addEventListener('cornerstoneimagerendered', (event) => {
+      // Assert
+      assert.equal(event.target, this.element);
+      done();
+    });
+
+    // Act
+    draw(this.element);
   });
 
   afterEach(function () {
