34eb4b701fd06b8b823126a60a7f614f845a3959
Zaid Safadi <zsafadi@hotmail.com>
2018-03-31 23:26:38 -0400

    Create a ""displayedArea" Example that loads the "displayed area" images from the IHE imagedisplay_testplan document. section 2.4.6

---
+++ example/exampleImageIdLoaderRaw.js
@@ -0,0 +1,65 @@
+(function (cs) {
+
+    "use strict";
+
+    function getExampleImage(imageId) {
+      let testCase = imageId.split('//')[1];
+
+        return {
+          promise: new Promise((resolve) => {
+            getImagePixel(testCase).then((imagePixel) => {
+              getImageData(testCase).then((imageData) => {
+                imageData.getPixelData = () => { return imagePixel; };
+
+                resolve(imageData);
+              });
+            });
+
+            }),
+            cancelFn: undefined
+        };
+
+        function getImagePixel(imageId) {
+          let promise = new Promise((resolve) => {
+            var req = new XMLHttpRequest();
+            req.onreadystatechange = function () {
+              if (this.readyState == 4 && this.status === 200 ) {
+                let imageRaw = req.responseText;
+                let pixelData = Uint8Array.from(atob(imageRaw), c => c.charCodeAt(0));
+
+                resolve(pixelData);
+              }
+            }
+            req.open('GET', baseUrl + imageId + '.txt', true);
+            req.send();
+          });
+
+          return promise;
+        }
+
+        function getImageData(imageId) {
+          let promise = new Promise((resolve) => {
+            var req = new XMLHttpRequest();
+            req.onreadystatechange = function () {
+              if (this.readyState === 4 && this.status === 200 ) {
+                  let imageData = req.responseText;
+
+                  let image = JSON.parse(imageData);
+
+                  resolve(image);
+              }
+            }
+            req.open('GET', baseUrl + imageId + '.json', true);
+            req.send();
+          });
+
+          return promise;
+        }
+    }
+
+    var baseUrl = '/example/displayedArea/test-cases/';
+
+    // register our imageLoader plugin with cornerstone
+    cs.registerImageLoader('example', getExampleImage);
+
+}(cornerstone));
