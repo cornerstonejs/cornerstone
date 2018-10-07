e53f05bd90b9cc721db4de41c884563cd9561115
diego0020 <gus0020@gmail.com>
2018-10-07 17:11:53 -0500

    add missing jsdocs

--- src/imageCache.js
+++ src/imageCache.js
@@ -15,7 +15,12 @@ const imageCacheDict = {};
 // Array of cachedImage objects
 export const cachedImages = [];
 
-export function setMaximumSizeBytes (numBytes) {
+/** Sets the maximum size of cache and purges cache contents if necessary. 
+ * 
+ * @param {number} numBytes The maximun size that the cache should occupy.
+ * @returns {void}
+ */
+export function setMaximumSizeBytes(numBytes) {
   if (numBytes === undefined) {
     throw new Error('setMaximumSizeBytes: parameter numBytes must not be undefined');
   }
@@ -29,8 +34,11 @@ export function setMaximumSizeBytes (numBytes) {
 
   purgeCacheIfNecessary();
 }
-
-function purgeCacheIfNecessary () {
+/**
+ * Purges the cache if size exceeds maximum
+ * @returns {void}
+ */
+function purgeCacheIfNecessary() {
   // If max cache size has not been exceeded, do nothing
   if (cacheSizeInBytes <= maximumSizeInBytes) {
     return;
@@ -38,7 +46,7 @@ function purgeCacheIfNecessary () {
 
   // Cache size has been exceeded, create list of images sorted by timeStamp
   // So we can purge the least recently used image
-  function compare (a, b) {
+  function compare(a, b) {
     if (a.timeStamp > b.timeStamp) {
       return -1;
     }
@@ -65,7 +73,13 @@ function purgeCacheIfNecessary () {
   triggerEvent(events, EVENTS.IMAGE_CACHE_FULL, cacheInfo);
 }
 
-export function putImageLoadObject (imageId, imageLoadObject) {
+/**
+ * Puts a new image loader into the cache
+ * 
+ * @param {string} imageId ImageId of the image loader
+ * @param {Object} imageLoadObject The object that is loading or loaded the image
+ */
+export function putImageLoadObject(imageId, imageLoadObject) {
   if (imageId === undefined) {
     throw new Error('putImageLoadObject: imageId must not be undefined');
   }
@@ -128,7 +142,12 @@ export function putImageLoadObject (imageId, imageLoadObject) {
   });
 }
 
-export function getImageLoadObject (imageId) {
+/**
+ * Retuns the object that is loading a given imageId
+ * 
+ * @param {string} imageId 
+ */
+export function getImageLoadObject(imageId) {
   if (imageId === undefined) {
     throw new Error('getImageLoadObject: imageId must not be undefined');
   }
@@ -144,7 +163,12 @@ export function getImageLoadObject (imageId) {
   return cachedImage.imageLoadObject;
 }
 
-export function removeImageLoadObject (imageId) {
+/**
+ * Removes the image loader associated with a given Id from the cache
+ * 
+ * @param {string} imageId 
+ */
+export function removeImageLoadObject(imageId) {
   if (imageId === undefined) {
     throw new Error('removeImageLoadObject: imageId must not be undefined');
   }
@@ -168,7 +192,17 @@ export function removeImageLoadObject (imageId) {
   delete imageCacheDict[imageId];
 }
 
-export function getCacheInfo () {
+/**
+ * @typedef {Object} CacheInformation
+ * @property {number} maximumSizeInBytes  The maximum size of the cache in bytes
+ * @property {number} cacheSizeInBytes Currently occupied space in the cache in bytes
+ * @property {number} numberOfImagesCached Number of ImageLoaders in the cache
+ */
+
+/**
+ * Gets the current state of the cache
+ */
+export function getCacheInfo() {
   return {
     maximumSizeInBytes,
     cacheSizeInBytes,
@@ -178,7 +212,12 @@ export function getCacheInfo () {
 
 // This method should only be called by `removeImageLoadObject` because it's
 // The one that knows how to deal with shared cache keys and cache size.
-function decache (imageLoadObject) {
+/**
+ * INTERNAL: Removes and ImageLoader from the cache
+ * 
+ * @param {Object} imageLoadObject 
+ */
+function decache(imageLoadObject) {
   imageLoadObject.promise.then(
     function () {
       if (imageLoadObject.decache) {
@@ -193,7 +232,10 @@ function decache (imageLoadObject) {
   );
 }
 
-export function purgeCache () {
+/**
+ * Removes all images from cache
+ */
+export function purgeCache() {
   while (cachedImages.length > 0) {
     const removedCachedImage = cachedImages[0];
 
@@ -201,7 +243,13 @@ export function purgeCache () {
   }
 }
 
-export function changeImageIdCacheSize (imageId, newCacheSize) {
+/**
+ * Updates the space than an image is using in the cache
+ * 
+ * @param {string} imageId 
+ * @param {number} newCacheSize 
+ */
+export function changeImageIdCacheSize(imageId, newCacheSize) {
   const cacheEntry = imageCacheDict[imageId];
 
   if (cacheEntry) {
