f6376c42d4786546a8d478c2796f486f7a453135
Kofifus <mikagenic@gmail.com>
2019-01-31 16:27:47 +1100

    DICOM overlays
    adds 'overlays' type returning an array of DICOM overlays
    
    http://dicom.nema.org/dicom/2013/output/chtml/part03/sect_C.9.html

--- src/internal/drawImageSync.js
+++ src/internal/drawImageSync.js
@@ -6,6 +6,48 @@ import { renderPseudoColorImage } from '../rendering/renderPseudoColorImage.js';
 import { renderLabelMapImage } from '../rendering/renderLabelMapImage.js';
 import triggerEvent from '../triggerEvent.js';
 import EVENTS from '../events.js';
+import getTransform from './getTransform.js';
+
+function renderOverlays(enabledElement, overlays) {
+  if (!overlays || overlays.length===0) return;
+
+  const imageWidth = Math.abs(enabledElement.viewport.displayedArea.brhc.x - enabledElement.viewport.displayedArea.tlhc.x) * enabledElement.viewport.displayedArea.columnPixelSpacing;
+  const imageHeight = Math.abs(enabledElement.viewport.displayedArea.brhc.y - enabledElement.viewport.displayedArea.tlhc.y) * enabledElement.viewport.displayedArea.rowPixelSpacing;
+
+  const context = enabledElement.canvas.getContext('2d');
+  context.save();
+
+  overlays.forEach(overlay => {
+    if (!overlay.visible) return;
+    const layerCanvas = document.createElement('canvas');
+    layerCanvas.width = imageWidth;
+    layerCanvas.height = imageHeight;
+    const layerContext = layerCanvas.getContext('2d');
+    const transform = getTransform(enabledElement);
+    layerContext.setTransform(transform.m[0], transform.m[1], transform.m[2], transform.m[3], transform.m[4], transform.m[5]);
+    layerContext.save();
+    layerContext.setTransform(1, 0, 0, 1, 0, 0);
+    layerContext.fillStyle = overlay.fillStyle;
+
+    if (overlay.type === 'R') {
+      layerContext.fillRect(0, 0, layerCanvas.width, layerCanvas.height);
+      layerContext.globalCompositeOperation = 'xor';
+    }
+
+    let i = 0;
+    for (let y = 0; y < overlay.rows; y++) {
+      for (let x = 0; x < overlay.columns; x++) {
+        if (overlay.pixelData[i++] > 0) {
+          layerContext.fillRect(x, y, 1, 1);
+        }
+      }
+    }
+    layerContext.restore();
+    context.drawImage(layerCanvas, 0, 0);  
+  });
+
+  context.restore();  
+}
 
 /**
  * Draw an image to a given enabled element synchronously
@@ -56,6 +98,7 @@ export default function (enabledElement, invalidated) {
     }
 
     render(enabledElement, invalidated);
+    renderOverlays(enabledElement, image.overlays);
   }
 
   // Calculate how long it took to draw the image/layers
