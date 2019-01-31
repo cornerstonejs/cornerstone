86126af261f4ef530e75f8389e15a764957e4ac1
Kofifus <mikagenic@gmail.com>
2019-02-01 08:02:59 +1100

    Update drawImageSync.js

--- src/internal/drawImageSync.js
+++ src/internal/drawImageSync.js
@@ -9,21 +9,28 @@ import EVENTS from '../events.js';
 import getTransform from './getTransform.js';
 
 function renderOverlays(enabledElement, overlays) {
-  if (!overlays || overlays.length===0) return;
+  if (!overlays || overlays.length === 0) {
+    return;
+  }
 
   const imageWidth = Math.abs(enabledElement.viewport.displayedArea.brhc.x - enabledElement.viewport.displayedArea.tlhc.x) * enabledElement.viewport.displayedArea.columnPixelSpacing;
   const imageHeight = Math.abs(enabledElement.viewport.displayedArea.brhc.y - enabledElement.viewport.displayedArea.tlhc.y) * enabledElement.viewport.displayedArea.rowPixelSpacing;
 
   const context = enabledElement.canvas.getContext('2d');
+
   context.save();
 
-  overlays.forEach(overlay => {
-    if (!overlay.visible) return;
+  overlays.forEach((overlay) => {
+    if (!overlay.visible) {
+      return;
+    }
     const layerCanvas = document.createElement('canvas');
+
     layerCanvas.width = imageWidth;
     layerCanvas.height = imageHeight;
     const layerContext = layerCanvas.getContext('2d');
     const transform = getTransform(enabledElement);
+
     layerContext.setTransform(transform.m[0], transform.m[1], transform.m[2], transform.m[3], transform.m[4], transform.m[5]);
     layerContext.save();
     layerContext.setTransform(1, 0, 0, 1, 0, 0);
@@ -35,6 +42,7 @@ function renderOverlays(enabledElement, overlays) {
     }
 
     let i = 0;
+    
     for (let y = 0; y < overlay.rows; y++) {
       for (let x = 0; x < overlay.columns; x++) {
         if (overlay.pixelData[i++] > 0) {
