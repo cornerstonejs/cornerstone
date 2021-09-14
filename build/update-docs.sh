24a167047fe896b715dd0977e16619798aaf6c63
swederik <erik.sweed@gmail.com>
2021-09-14 10:19:20 +0200

    ci: try to fix gitbook build and output

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -1,9 +1,15 @@
 cd docs
 rm -rf _book
+mkdir -p _book
 gitbook install
 gitbook build
 cp assets/CNAME _book/CNAME
 cd _book
+
+# Set User
+git config --global user.email "erik.sweed@gmail.com"
+git config --global user.name "swederik"
+
 git init
 git add -A
 git commit -m 'Update compiled GitBook (this commit is automatic)'
