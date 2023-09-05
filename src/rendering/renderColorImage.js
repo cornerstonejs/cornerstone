ce1f2ed37b0bc078b999c3266be27cd2235813ba
adzqq <zqq@ituji.cn>
2023-09-05 11:49:29 +0800

    格式化，no change

--- src/rendering/renderColorImage.js
+++ src/rendering/renderColorImage.js
@@ -20,22 +20,22 @@ import getDisplayedArea from '../internal/getDisplayedArea.js';
  * @returns {Uint8ClampedArray} Look Up Table array.
  * @memberof rendering
  */
-function getLut (image, viewport) {
-  // If we have a cached lut and it has the right values, return it immediately
-  if (image.cachedLut !== undefined &&
-    image.cachedLut.windowCenter === viewport.voi.windowCenter &&
-    image.cachedLut.windowWidth === viewport.voi.windowWidth &&
-    image.cachedLut.invert === viewport.invert) {
+function getLut(image, viewport) {
+    // If we have a cached lut and it has the right values, return it immediately
+    if (image.cachedLut !== undefined &&
+        image.cachedLut.windowCenter === viewport.voi.windowCenter &&
+        image.cachedLut.windowWidth === viewport.voi.windowWidth &&
+        image.cachedLut.invert === viewport.invert) {
+        return image.cachedLut.lutArray;
+    }
+
+    // Lut is invalid or not present, regenerate it and cache it
+    generateColorLut(image, viewport.voi.windowWidth, viewport.voi.windowCenter, viewport.invert);
+    image.cachedLut.windowWidth = viewport.voi.windowWidth;
+    image.cachedLut.windowCenter = viewport.voi.windowCenter;
+    image.cachedLut.invert = viewport.invert;
+
     return image.cachedLut.lutArray;
-  }
-
-  // Lut is invalid or not present, regenerate it and cache it
-  generateColorLut(image, viewport.voi.windowWidth, viewport.voi.windowCenter, viewport.invert);
-  image.cachedLut.windowWidth = viewport.voi.windowWidth;
-  image.cachedLut.windowCenter = viewport.voi.windowCenter;
-  image.cachedLut.invert = viewport.invert;
-
-  return image.cachedLut.lutArray;
 }
 
 /**
@@ -48,61 +48,61 @@ function getLut (image, viewport) {
  * @returns {HTMLCanvasElement} An appropriate canvas for rendering the image
  * @memberof rendering
  */
-function getRenderCanvas (enabledElement, image, invalidated) {
-  const canvasWasColor = enabledElement.renderingTools.lastRenderedIsColor === true;
+function getRenderCanvas(enabledElement, image, invalidated) {
+    const canvasWasColor = enabledElement.renderingTools.lastRenderedIsColor === true;
 
-  if (!enabledElement.renderingTools.renderCanvas || !canvasWasColor) {
-    enabledElement.renderingTools.renderCanvas = document.createElement('canvas');
-  }
+    if (!enabledElement.renderingTools.renderCanvas || !canvasWasColor) {
+        enabledElement.renderingTools.renderCanvas = document.createElement('canvas');
+    }
 
-  const renderCanvas = enabledElement.renderingTools.renderCanvas;
+    const renderCanvas = enabledElement.renderingTools.renderCanvas;
 
-  // The ww/wc is identity and not inverted - get a canvas with the image rendered into it for
-  // Fast drawing
-  if (enabledElement.viewport.voi.windowWidth === 255 &&
-    enabledElement.viewport.voi.windowCenter === 128 &&
-    enabledElement.viewport.invert === false &&
-    image.getCanvas &&
-    image.getCanvas()
-  ) {
-    return image.getCanvas();
-  }
+    // The ww/wc is identity and not inverted - get a canvas with the image rendered into it for
+    // Fast drawing
+    if (enabledElement.viewport.voi.windowWidth === 255 &&
+        enabledElement.viewport.voi.windowCenter === 128 &&
+        enabledElement.viewport.invert === false &&
+        image.getCanvas &&
+        image.getCanvas()
+    ) {
+        return image.getCanvas();
+    }
 
-  // Apply the lut to the stored pixel data onto the render canvas
-  if (doesImageNeedToBeRendered(enabledElement, image) === false && invalidated !== true) {
+    // Apply the lut to the stored pixel data onto the render canvas
+    if (doesImageNeedToBeRendered(enabledElement, image) === false && invalidated !== true) {
+        return renderCanvas;
+    }
+
+    // If our render canvas does not match the size of this image reset it
+    // NOTE: This might be inefficient if we are updating multiple images of different
+    // Sizes frequently.
+    if (renderCanvas.width !== image.width || renderCanvas.height !== image.height) {
+        initializeRenderCanvas(enabledElement, image);
+    }
+
+    // Get the lut to use
+    let start = now();
+    const colorLut = getLut(image, enabledElement.viewport);
+
+    image.stats = image.stats || {};
+    image.stats.lastLutGenerateTime = now() - start;
+
+    const renderCanvasData = enabledElement.renderingTools.renderCanvasData;
+    const renderCanvasContext = enabledElement.renderingTools.renderCanvasContext;
+
+    // The color image voi/invert has been modified - apply the lut to the underlying
+    // Pixel data and put it into the renderCanvas
+    if (image.rgba) {
+        storedRGBAPixelDataToCanvasImageData(image, colorLut, renderCanvasData.data);
+    } else {
+        storedColorPixelDataToCanvasImageData(image, colorLut, renderCanvasData.data);
+    }
+
+    start = now();
+    renderCanvasContext.putImageData(renderCanvasData, 0, 0);
+    image.stats.lastPutImageDataTime = now() - start;
+
     return renderCanvas;
-  }
-
-  // If our render canvas does not match the size of this image reset it
-  // NOTE: This might be inefficient if we are updating multiple images of different
-  // Sizes frequently.
-  if (renderCanvas.width !== image.width || renderCanvas.height !== image.height) {
-    initializeRenderCanvas(enabledElement, image);
-  }
-
-  // Get the lut to use
-  let start = now();
-  const colorLut = getLut(image, enabledElement.viewport);
-
-  image.stats = image.stats || {};
-  image.stats.lastLutGenerateTime = now() - start;
-
-  const renderCanvasData = enabledElement.renderingTools.renderCanvasData;
-  const renderCanvasContext = enabledElement.renderingTools.renderCanvasContext;
-
-  // The color image voi/invert has been modified - apply the lut to the underlying
-  // Pixel data and put it into the renderCanvas
-  if (image.rgba) {
-    storedRGBAPixelDataToCanvasImageData(image, colorLut, renderCanvasData.data);
-  } else {
-    storedColorPixelDataToCanvasImageData(image, colorLut, renderCanvasData.data);
-  }
-
-  start = now();
-  renderCanvasContext.putImageData(renderCanvasData, 0, 0);
-  image.stats.lastPutImageDataTime = now() - start;
-
-  return renderCanvas;
 }
 
 /**
@@ -113,81 +113,81 @@ function getRenderCanvas (enabledElement, image, invalidated) {
  * @returns {void}
  * @memberof rendering
  */
-export function renderColorImage (enabledElement, invalidated) {
-  if (enabledElement === undefined) {
-    throw new Error('renderColorImage: enabledElement parameter must not be undefined');
-  }
+export function renderColorImage(enabledElement, invalidated) {
+    if (enabledElement === undefined) {
+        throw new Error('renderColorImage: enabledElement parameter must not be undefined');
+    }
 
-  const image = enabledElement.image;
+    const image = enabledElement.image;
 
-  if (image === undefined) {
-    throw new Error('renderColorImage: image must be loaded before it can be drawn');
-  }
+    if (image === undefined) {
+        throw new Error('renderColorImage: image must be loaded before it can be drawn');
+    }
 
-  // Get the canvas context and reset the transform
-  const context = enabledElement.canvas.getContext('2d', {
-    desynchronized: true
-  });
+    // Get the canvas context and reset the transform
+    const context = enabledElement.canvas.getContext('2d', {
+        desynchronized: true
+    });
 
-  context.setTransform(1, 0, 0, 1, 0, 0);
+    context.setTransform(1, 0, 0, 1, 0, 0);
 
-  // Clear the canvas
-  context.fillStyle = getFillStyle(enabledElement);
-  context.fillRect(0, 0, enabledElement.canvas.width, enabledElement.canvas.height);
+    // Clear the canvas
+    context.fillStyle = getFillStyle(enabledElement);
+    context.fillRect(0, 0, enabledElement.canvas.width, enabledElement.canvas.height);
 
-  // Turn off image smooth/interpolation if pixelReplication is set in the viewport
-  context.imageSmoothingEnabled = !enabledElement.viewport.pixelReplication;
-  context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
+    // Turn off image smooth/interpolation if pixelReplication is set in the viewport
+    context.imageSmoothingEnabled = !enabledElement.viewport.pixelReplication;
+    context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
 
-  // Save the canvas context state and apply the viewport properties
-  setToPixelCoordinateSystem(enabledElement, context);
+    // Save the canvas context state and apply the viewport properties
+    setToPixelCoordinateSystem(enabledElement, context);
 
-  let renderCanvas;
+    let renderCanvas;
 
-  if (enabledElement.options && enabledElement.options.renderer &&
-    enabledElement.options.renderer.toLowerCase() === 'webgl') {
-    // If this enabled element has the option set for WebGL, we should
-    // User it as our renderer.
-    renderCanvas = webGL.renderer.render(enabledElement);
-  } else {
-    // If no options are set we will retrieve the renderCanvas through the
-    // Normal Canvas rendering path
-    renderCanvas = getRenderCanvas(enabledElement, image, invalidated);
-  }
+    if (enabledElement.options && enabledElement.options.renderer &&
+        enabledElement.options.renderer.toLowerCase() === 'webgl') {
+        // If this enabled element has the option set for WebGL, we should
+        // User it as our renderer.
+        renderCanvas = webGL.renderer.render(enabledElement);
+    } else {
+        // If no options are set we will retrieve the renderCanvas through the
+        // Normal Canvas rendering path
+        renderCanvas = getRenderCanvas(enabledElement, image, invalidated);
+    }
 
-  const imageDisplayedArea = getDisplayedArea(enabledElement.image, enabledElement.viewport);
-  const sx = imageDisplayedArea.tlhc.x - 1;
-  const sy = imageDisplayedArea.tlhc.y - 1;
-  const width = imageDisplayedArea.brhc.x - sx;
-  const height = imageDisplayedArea.brhc.y - sy;
+    const imageDisplayedArea = getDisplayedArea(enabledElement.image, enabledElement.viewport);
+    const sx = imageDisplayedArea.tlhc.x - 1;
+    const sy = imageDisplayedArea.tlhc.y - 1;
+    const width = imageDisplayedArea.brhc.x - sx;
+    const height = imageDisplayedArea.brhc.y - sy;
 
-  context.drawImage(renderCanvas, sx, sy, width, height, sx, sy, width, height);
+    context.drawImage(renderCanvas, sx, sy, width, height, sx, sy, width, height);
 
-  enabledElement.renderingTools = saveLastRendered(enabledElement);
+    enabledElement.renderingTools = saveLastRendered(enabledElement);
 }
 
-export function addColorLayer (layer, invalidated) {
-  if (layer === undefined) {
-    throw new Error('addColorLayer: layer parameter must not be undefined');
-  }
+export function addColorLayer(layer, invalidated) {
+    if (layer === undefined) {
+        throw new Error('addColorLayer: layer parameter must not be undefined');
+    }
 
-  const image = layer.image;
+    const image = layer.image;
 
-  if (image === undefined) {
-    throw new Error('addColorLayer: image must be loaded before it can be drawn');
-  }
+    if (image === undefined) {
+        throw new Error('addColorLayer: image must be loaded before it can be drawn');
+    }
 
-  // All multi-layer images should include the alpha value
-  image.rgba = true;
-  layer.canvas = getRenderCanvas(layer, image, invalidated);
+    // All multi-layer images should include the alpha value
+    image.rgba = true;
+    layer.canvas = getRenderCanvas(layer, image, invalidated);
 
-  const context = layer.canvas.getContext('2d', {
-    desynchronized: true
-  });
+    const context = layer.canvas.getContext('2d', {
+        desynchronized: true
+    });
 
-  // Turn off image smooth/interpolation if pixelReplication is set in the viewport
-  context.imageSmoothingEnabled = !layer.viewport.pixelReplication;
-  context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
+    // Turn off image smooth/interpolation if pixelReplication is set in the viewport
+    context.imageSmoothingEnabled = !layer.viewport.pixelReplication;
+    context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
 
-  layer.renderingTools = saveLastRendered(layer);
+    layer.renderingTools = saveLastRendered(layer);
 }
