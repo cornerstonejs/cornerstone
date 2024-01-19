154e64bea71427708387f24c680fa43ecbcba89a
cw <1096486917@qq.com>
2024-01-19 17:36:47 +0800

    Compatible with @Cornerstonejs/dicom-image-loader
    Compatible with @Cornerstonejs/dicom-image-loader

--- src/rendering/renderColorImage.js
+++ src/rendering/renderColorImage.js
@@ -59,11 +59,10 @@ function getRenderCanvas (enabledElement, image, invalidated) {
 
   // The ww/wc is identity and not inverted - get a canvas with the image rendered into it for
   // Fast drawing
-  if (enabledElement.viewport.voi.windowWidth === 255 &&
-    enabledElement.viewport.voi.windowCenter === 128 &&
+  if ((enabledElement.viewport.voi.windowWidth === 255 || enabledElement.viewport.voi.windowWidth === 256) &&
+    (enabledElement.viewport.voi.windowCenter === 127 || enabledElement.viewport.voi.windowCenter === 128) &&
     enabledElement.viewport.invert === false &&
-    image.getCanvas &&
-    image.getCanvas()
+    image.getCanvas && image.getCanvas()
   ) {
     return image.getCanvas();
   }
